3a079c285d7b5be6c6e358b8767254d7b978e60c
Qualeke Singleton <squaleke@yahoo.com>
2021-12-06 01:40:17 -0500

    laracast

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <link rel="stylesheet" href="app.css">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+   <title>Document</title>
+</head>
+<body>
+   <article>
+    <h1><a  style=" text-decoration: none; color: black;" href="/post">First Post</h1></a>
+   <p>Fizzle ipsum dolor sit uhuh ... yih!, go to hizzle adipiscing elit. Crackalackin dizzle velizzle, fo shizzle volutpizzle, the bizzle quis, brizzle vizzle, arcu. Pellentesque eget ma nizzle. Sizzle eros. Mammasay mammasa mamma oo sa bling bling dolor dapibizzle turpis tempus bow wow wow. Maurizzle doggy get down get down et turpizzle. Vestibulum fo shizzle mah nizzle fo rizzle, mah home g-dizzle tortizzle. Pellentesque eleifend rhoncizzle break yo neck, yall. Crunk tellivizzle habitasse we gonna chung dictumst. Yo dapibizzle. Curabitur tellizzle mofo, pretium eu, mattizzle ac, shizzlin dizzle shiz, nunc. Break yo neck, yall suscipizzle. Uhuh ... yih! sempizzle velit sizzle fizzle.</p>
+
+</article>
+   <article>
+   <h1> <a  style=" text-decoration: none; color: black;" href="/post"> Second Post</h1></a>
+   <p>Fizzle ipsum dolor sit uhuh ... yih!, go to hizzle adipiscing elit. Crackalackin dizzle velizzle, fo shizzle volutpizzle, the bizzle quis, brizzle vizzle, arcu. Pellentesque eget ma nizzle. Sizzle eros. Mammasay mammasa mamma oo sa bling bling dolor dapibizzle turpis tempus bow wow wow. Maurizzle doggy get down get down et turpizzle. Vestibulum fo shizzle mah nizzle fo rizzle, mah home g-dizzle tortizzle. Pellentesque eleifend rhoncizzle break yo neck, yall. Crunk tellivizzle habitasse we gonna chung dictumst. Yo dapibizzle. Curabitur tellizzle mofo, pretium eu, mattizzle ac, shizzlin dizzle shiz, nunc. Break yo neck, yall suscipizzle. Uhuh ... yih! sempizzle velit sizzle fizzle.</p>
+   </article>
+
+   <article>
+   <h1> <a  style=" text-decoration: none; color: black;" href="/post"> Third Post</h1> </a>
+   <p>Fizzle ipsum dolor sit uhuh ... yih!, go to hizzle adipiscing elit. Crackalackin dizzle velizzle, fo shizzle volutpizzle, the bizzle quis, brizzle vizzle, arcu. Pellentesque eget ma nizzle. Sizzle eros. Mammasay mammasa mamma oo sa bling bling dolor dapibizzle turpis tempus bow wow wow. Maurizzle doggy get down get down et turpizzle. Vestibulum fo shizzle mah nizzle fo rizzle, mah home g-dizzle tortizzle. Pellentesque eleifend rhoncizzle break yo neck, yall. Crunk tellivizzle habitasse we gonna chung dictumst. Yo dapibizzle. Curabitur tellizzle mofo, pretium eu, mattizzle ac, shizzlin dizzle shiz, nunc. Break yo neck, yall suscipizzle. Uhuh ... yih! sempizzle velit sizzle fizzle.</p>
+   </article>
+</body>
+</html>
